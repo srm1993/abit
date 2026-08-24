912bf920b4aaad6427a109399487803f799774ce
Soumyaranjan Mahalik <124546914+srm1993@users.noreply.github.com>
2026-08-24 23:08:59 +0530

    Refactor edit.php for improved readability and security

--- edit.php
+++ edit.php
@@ -1,38 +1,112 @@
 <?php
-    include('config.php');
-    $id=$_REQUEST['id'];
-    $sql=mysqli_query($con,"Select * from student where id='$id'");
-    $data=mysqli_fetch_assoc($sql);
-    if($_SERVER['REQUEST_METHOD']=='POST'){
-        $id=$_REQUEST['id'];
-        $name=$_REQUEST['name'];
-        $email=$_REQUEST['email'];
-        $age=$_REQUEST['age'];
-        $gender=$_REQUEST['gender'];
-        $phone=$_REQUEST['phone'];
-        $mySql=mysqli_query($con,"Update student set name='$name',email='$email',age='$age',gender='$gender',phone='$phone' where id='$id'");
-        if(mysqli_affected_rows($con)>0){
-            header('location:student.php');
-        }else{
-           echo "something went wrong"; 
-        }
+
+include('config.php');
+
+$id = $_REQUEST['id'];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $phone = $_POST['phone'];
+
+    $sql = "UPDATE student 
+            SET name='$name',
+                email='$email',
+                age='$age',
+                gender='$gender',
+                phone='$phone'
+            WHERE id='$id'";
+
+    $result = mysqli_query($con, $sql);
+
+    if ($result) {
+        header('Location: student.php');
+        exit;
+    } else {
+        echo "Something went wrong: " . mysqli_error($con);
     }
+}
+
+/* Get student data */
+$sql = mysqli_query(
+    $con,
+    "SELECT * FROM student WHERE id='$id'"
+);
+
+$data = mysqli_fetch_assoc($sql);
+
 ?>
+
 <form method="post">
-    <input type="text" name="name" placeholder="Enter Name" value="<?php echo $data['name']; ?>">
+
+    <input 
+        type="text" 
+        name="name" 
+        placeholder="Enter Name"
+        value="<?php echo htmlspecialchars($data['name']); ?>"
+    >
+
     <br>
-    <input type="email" name="email" placeholder="Enter Email" value="<?php echo $data['email']; ?>">
+
+    <input 
+        type="email" 
+        name="email" 
+        placeholder="Enter Email"
+        value="<?php echo htmlspecialchars($data['email']); ?>"
+    >
+
     <br>
-    <input type="text" name="age" placeholder="Enter Age" value="<?php echo $data['age']; ?>">
+
+    <input 
+        type="text" 
+        name="age" 
+        placeholder="Enter Age"
+        value="<?php echo htmlspecialchars($data['age']); ?>"
+    >
+
     <br>
+
     <select name="gender">
+
         <option hidden>Choose Your Gender</option>
-        <option value="male" <?php if($data['gender']=='male'){?>selected<?php } ?>>Male</option>
-        <option value="female" <?php if($data['gender']=='female'){?>selected<?php } ?>>Female</option>
-        <option value="other" <?php if($data['gender']=='others'){?>selected<?php } ?>>Others</option>
+
+        <option 
+            value="male"
+            <?php if ($data['gender'] == 'male') echo 'selected'; ?>
+        >
+            Male
+        </option>
+
+        <option 
+            value="female"
+            <?php if ($data['gender'] == 'female') echo 'selected'; ?>
+        >
+            Female
+        </option>
+
+        <option 
+            value="other"
+            <?php if ($data['gender'] == 'other') echo 'selected'; ?>
+        >
+            Others
+        </option>
+
     </select>
+
     <br>
-    <input type="text" name="phone" placeholder="Enter Phone" value="<?php echo $data['phone']; ?>">
+
+    <input 
+        type="text" 
+        name="phone" 
+        placeholder="Enter Phone"
+        value="<?php echo htmlspecialchars($data['phone']); ?>"
+    >
+
     <br>
-    <input type="submit" name="btn" >
+
+    <input type="submit" value="Update">
+
 </form>
