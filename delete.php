55339e0fc7d4ea37fed91d8f6a80bde3177d9982
Soumyaranjan Mahalik <124546914+srm1993@users.noreply.github.com>
2026-08-24 23:16:57 +0530

    Refactor delete.php for proper redirection
    Added exit statement after redirecting to index.php.

--- delete.php
+++ delete.php
@@ -1,6 +1,13 @@
 <?php
-    include('config.php');
-    $id=$_GET['id'];
-    $sql=mysqli_query($con,"DELETE FROM student WHERE id='$id'");
-    header("location:index.php");
-?>
+
+include('config.php');
+
+$id = $_GET['id'];
+
+$sql = mysqli_query(
+    $con,
+    "DELETE FROM student WHERE id='$id'"
+);
+
+header("Location: index.php");
+exit;
