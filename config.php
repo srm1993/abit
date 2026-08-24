691498b089670075c2083ef93e0cc4a2e4e94b55
Soumyaranjan Mahalik <124546914+srm1993@users.noreply.github.com>
2026-08-24 22:57:38 +0530

    Add port number to MySQL connection

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-    $con=mysqli_connect("mysql-3ac8eab-soumyaseeree-b20d.b.aivencloud.com","avnadmin","AVNS_9eD7ubCuGxgHtma8ggE","defaultdb");
+    $con=mysqli_connect("mysql-3ac8eab-soumyaseeree-b20d.b.aivencloud.com","avnadmin","AVNS_9eD7ubCuGxgHtma8ggE","defaultdb",19271);
     if(!$con){
         die("Something Went Wrong");
     }
