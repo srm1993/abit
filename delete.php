d7a0d34196174c21789d7e00cdee31a1811d63ff
Soumyaranjan Mahalik <124546914+srm1993@users.noreply.github.com>
2026-08-24 23:05:02 +0530

    Fix table name case in delete query

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php
     include('config.php');
     $id=$_GET['id'];
-    $sql=mysqli_query($con,"DELETE FROM STUDENT WHERE id='$id'");
+    $sql=mysqli_query($con,"DELETE FROM student WHERE id='$id'");
     header("location:student.php");
 ?>
