0fd623e78f250a080e23a4ba706df841f4b897ea
Soumyaranjan Mahalik <124546914+srm1993@users.noreply.github.com>
2026-08-24 23:12:17 +0530

    Redirect to index.php after deletion

--- delete.php
+++ delete.php
@@ -2,5 +2,5 @@
     include('config.php');
     $id=$_GET['id'];
     $sql=mysqli_query($con,"DELETE FROM student WHERE id='$id'");
-    header("location:student.php");
+    header("location:index.php");
 ?>
