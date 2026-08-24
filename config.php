e5a08a14afcefa9a4d8e1248c6153fb91561b46f
Soumyaranjan Mahalik <124546914+srm1993@users.noreply.github.com>
2026-08-24 23:02:28 +0530

    Refactor database connection in config.php
    Refactored database connection code to use mysqli_init and SSL settings.

--- config.php
+++ config.php
@@ -1,6 +1,32 @@
 <?php
-    $con=mysqli_connect("mysql-3ac8eab-soumyaseeree-b20d.b.aivencloud.com","avnadmin","AVNS_9eD7ubCuGxgHtma8ggE","defaultdb",19271);
-    if(!$con){
-        die("Something Went Wrong");
-    }
+
+$host = "mysql-3ac8eab-soumyaseeree-b20d.b.aivencloud.com";
+$user = "avnadmin";
+$password = "AVNS_9eD7ubCuGxgHtma8ggE";
+$database = "defaultdb";
+$port = 19271;
+
+$con = mysqli_init();
+
+mysqli_ssl_set(
+    $con,
+    null,
+    null,
+    null,
+    null,
+    null
+);
+
+if (!mysqli_real_connect(
+    $con,
+    $host,
+    $user,
+    $password,
+    $database,
+    $port
+)) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+echo "Database connected successfully!";
 ?>
